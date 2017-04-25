c09e83aa322034265db7753224aab406cc9b693d
Nataliya Kryvych <nkryvych@gmail.com>
2017-04-25 15:34:20 +0200

    removed obsolete directory

--- src/main�java/ch/epfl/cryos/osper/station/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains the {@link ch.epfl.cryos.osper.station.Application} class to run the application
- * with built in Tomcat and global field definitions. 
- */
-package ch.epfl.cryos.osper.station;
